1771410dc60a9913987bc1816a2a25b3e5ea9030
Paul <2381022@unai.edu>
2025-01-20 08:35:12 +0700

    bug fix

--- main-v2.ts
+++ main-v2.ts
@@ -5,14 +5,14 @@ interface Human {
     motor: string;
   }
   
-  let objectHuman: Human = {
+  let objectHuman2: Human = {
     name: "John",
     age: 20,
     location: "Jakarta",
     motor: "Honda",
   };
   
-  function printHuman(human: Human): void {
+  function printHuman2(human: Human): void {
     console.log(human.location);
   }
   
